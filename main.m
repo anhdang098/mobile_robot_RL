47e6070d47bcdec396a15fb7c7978d43e92c4707
Dang Anh <songah8@gmail.com>
2021-03-24 15:30:29 +0700

    misc updates

--- main.m
+++ main.m
@@ -2,4 +2,6 @@ clc, clear, close all;
 
 % Initialize
 rbt = Robot(10, 5, 0.6, 0.12, 0.001, 0.2);
-[fig, components] = rbt.init_figure();
+rbt.obs(3, :) = [2, 2];
+rbt = rbt.observe_state();
+rbt = rbt.get_state_property();
